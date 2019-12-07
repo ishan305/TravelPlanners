4f4fe13d86451c05645675cf28fa884faccebc91
abhinavp403 <40380390+abhinavp403@users.noreply.github.com>
2019-12-06 22:16:42 -0500

    Add files via upload

--- 326_Project/A2.js
+++ 326_Project/A2.js
@@ -1,3 +1,83 @@
+/*var app = require("express")(); 
+var bodyParser = require("body-parser"); 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://abhinavp403:tplanners123@travelplanner-kpu15.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+var arr1 = [];
+var arr2 = [];
+var arr3 = [];
+var t1 = new Map();
+var t2 = new Map();
+var t3 = new Map();
+var d1 = new Map();
+var d2 = new Map();
+var d3 = new Map();
+
+//Set view engine to ejs
+app.set("view engine", "ejs"); 
+
+//Tell Express where we keep our index.ejs
+app.set("views", __dirname + "/views"); 
+
+//Use body-parser
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+//Instead of sending Hello World, we render index.ejs
+//app.get("/", (req, res) => { res.render("index") }); 
+app.get("/", (req, res) => {
+    
+});
+
+app.post("/", function (req, res) {
+    //console.log(req.body['day']);
+    var day = req.body['day'];
+    MongoClient.connect(uri, function(err, db) {
+        client.connect(err => {
+            console.log('Connected...');
+            const collection1 = client.db("TravelPlanner").collection("Restaurants");
+            const collection2 = client.db("TravelPlanner").collection("Sites");
+            const collection3 = client.db("TravelPlanner").collection("Bars");
+            collection1.find().toArray(function(err, doc) {
+                if (doc == null)
+                    db.close();
+                for(var i=0; i<doc.length; i++)
+                    //console.log(doc[i]["Name"] + "       " + doc[i][day]);
+                    if(doc[i][day] !== "") {
+                        arr1.push(doc[i]["Name"]);
+                        t1.set(doc[i]["Name"], doc[i][day]);
+                        d1.set(doc[i]["Name"], doc[i]["Duration"]);
+                    }
+            });
+            collection2.find().toArray(function(err, doc) {
+                if (doc == null)
+                    db.close();
+                for(var i=0; i<doc.length; i++)
+                    if(doc[i][day] !== "") {
+                        arr2.push(doc[i]["Name"]);
+                        t2.set(doc[i]["Name"], doc[i][day]);
+                        d2.set(doc[i]["Name"], doc[i]["Duration"]);
+                    }
+            });
+            collection3.find().toArray(function(err, doc) {
+                if (doc == null)
+                    db.close();
+                for(var i=0; i<doc.length; i++)
+                    if(doc[i][day] !== "") {
+                        arr3.push(doc[i]["Name"]);
+                        t3.set(doc[i]["Name"], doc[i][day]);
+                        d3.set(doc[i]["Name"], doc[i]["Duration"]);
+                    }
+            });
+        });
+    });
+    console.log(t3);
+    console.log(d3.get("Hangar")); //check line 157
+    res.render("index", { restaurants: arr1, sites: arr2, bars: arr3, restaurants_timings: t1, sites_timings: t2, bars_timings: t3, restaurants_duration: d1, sites_duration: d2, bars_duration: d3}); 
+});
+
+app.listen(8080, function() { console.log("Server online on http://localhost:8080"); });*/
+
+
 var app = require("express")(); 
 var bodyParser = require("body-parser"); 
 const MongoClient = require('mongodb').MongoClient;
